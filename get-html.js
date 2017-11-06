c70b2f5d4a05555dbeb61ba69f1ec21dbb2f9c0b
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-06 20:06:28 +0000

    write a seperate function that gets the HTML

---
+++ get-html.js
@@ -0,0 +1,29 @@
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  const https = require('https');
+  let data = '';
+
+  https.get(options, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function(chunks){
+      data += chunks;
+    });
+
+    response.on('end', function(){
+      callback(data);
+    })
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
